19bdbdb90c96a2aab6961bb83edc021194470aea
VinyLimaZ <vinylimaz@live.com>
2016-11-15 18:54:07 -0200

    User create a job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,21 @@ class JobsController < ApplicationController
   def show
     @job = Job.find params[:id]
   end
+
+  def new
+    @job = Job.new
+  end
+
+  def create
+    @job = Job.new jobs_params
+    @job.save
+    redirect_to job_path(@job)
+  end
+
+  private
+
+    def jobs_params
+      params.require(:job).permit(:title, :company, :location, :category, :description)
+    end
+
 end
