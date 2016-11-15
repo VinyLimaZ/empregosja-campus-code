8fafc6c822825be0386b5259e501b6e24246e0ae
VinyLimaZ <vinylimaz@live.com>
2016-11-15 20:49:36 -0200

    User use companies for register a new job

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,6 +6,7 @@ class JobsController < ApplicationController
 
   def new
     @job = Job.new
+    @companies = Company.all
   end
 
   def create
@@ -17,7 +18,7 @@ class JobsController < ApplicationController
   private
 
     def jobs_params
-      params.require(:job).permit(:title, :company, :location, :category, :description, :featured)
+      params.require(:job).permit(:title, :company_id, :location, :category, :description, :featured)
     end
 
 end
