291a54894566420aa56be3f1088555f99c88edb0
VinyLimaZ <vinylimaz@live.com>
2016-11-20 20:41:41 -0200

    User dont see a job that have more than 90 days

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,4 +4,8 @@ class Job < ApplicationRecord
 
   validates :title, :location, :description, :category, :company, presence: true
 
+  def expired?
+    self.created_at < (Time.now - 90.days)
+  end
+
 end
